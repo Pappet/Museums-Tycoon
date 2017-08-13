5e19506c1d5e033c3255b760dd3aba92a2af6348
Pappet <pappet@web.de>
2017-08-13 23:19:34 +0200

    Basic Work
    Museums and Room Setup.

---
+++ Museums Tycoon 3D/Assets/Scripts/Museum.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Museum : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public GameObject RoomPrefab;
+
+	Room[,] rooms;
+
+	// Use this for initialization
+	void Start () {
+		rooms = new Room[width, height];
+		for(int x = 0; x < width; x++){
+			for(int z = 0; z < height; z++){
+				Instantiate (RoomPrefab, new Vector3(x,0,z), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
