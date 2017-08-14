f168db6fc25f6b50ca975ce229642cd90cfd9273
Pappet <pappet@web.de>
2017-08-14 11:30:13 +0200

    Changed to Handcrafting
    No Code Level Generation. Make Objects and place them.

--- Museums Tycoon 3D/Assets/Scripts/Museum.cs
+++ Museums Tycoon 3D/Assets/Scripts/Museum.cs
@@ -4,31 +4,14 @@ using UnityEngine;
 
 public class Museum : MonoBehaviour {
 
-	public int width;
-	public int height;
-	public GameObject RoomPrefab;
-	Room[,] rooms;
 
 	// Use this for initialization
 	void Start () {
-		rooms = new Room[width, height];
-		for(int x = 0; x < width; x++){
-			for(int y = 0; y < height; y++){
-				GameObject go = Instantiate (RoomPrefab, new Vector3(x,0,y), Quaternion.identity);
-				go.name = "Room x:" + x + "/ y:" + y;
-				go.transform.SetParent(this.transform);
-				rooms [x, y] = go.GetComponent<Room> ();
-				rooms [x, y].SetUpWalls (x,y);
-			}
-		}
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-
-	public Room ReturnRandomRoom(){
-		return rooms[Random.Range(0,width),Random.Range(0,height)];
 	}
 }
--- Museums Tycoon 3D/Assets/Scripts/Visitor.cs
+++ Museums Tycoon 3D/Assets/Scripts/Visitor.cs
@@ -4,42 +4,15 @@ using UnityEngine;
 
 public class Visitor : MonoBehaviour {
 
-	Museum museum;
-	Transform Waypoint;
-
 	public float speed;
-
-
-	void Awake(){
-		museum = FindObjectOfType<Museum> ();
-	}
 
 	// Use this for initialization
 	void Start () {
-		Waypoint = museum.ReturnRandomRoom ().transform;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ReachedWaypoint ();
 
-	}
-
-	void MoveTowardsWaypoint(){
-		Vector3 pos = transform.position;
-		float step = speed * Time.deltaTime;
-
-		pos =  Vector3.Lerp(transform.position,Waypoint.position,step);
-		pos.y = 0.15f;
-
-		transform.position = pos;
-	}
-
-	void ReachedWaypoint(){
-		if (Vector3.Distance (transform.position, Waypoint.position) > 0.5f) {
-			MoveTowardsWaypoint ();
-		} else {
-			Waypoint = museum.ReturnRandomRoom ().transform;
-		}
 	}
 }
